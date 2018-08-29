ffd852376de37209f524b44147048b78a81bda34
tanyashanker <tanya.shanker.3@gmail.com>
2018-08-29 12:32:18 +0530

    first commit

---
+++ src/main/java/com/capgemini/bechdalo/group7/Group7Application.java
@@ -0,0 +1,12 @@
+package com.capgemini.bechdalo.group7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group7Application.class, args);
+	}
+}
